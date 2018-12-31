3f6c390e3123bec5a2235d0adaa7d8de1488c9d6
wuxianqiang <2631640352@qq.com>
2018-12-31 21:36:44 +0800

    处理then中的返回值

--- day03/case1.js
+++ day03/case1.js
@@ -1,22 +1,31 @@
 let Promise = require('./promise2')
 
-let p = new Promise((resolve, reject) => {
-  // throw new Error('出错')
-  setTimeout(() => {
-    reject('出错')
-  }, 3000)
-})
+// let p = new Promise((resolve, reject) => {
+//   // throw new Error('出错')
+//   setTimeout(() => {
+//     reject('出错')
+//   }, 3000)
+// })
 
-p.then((value) => {
-  console.log(value)
-}, (reason) => {
-  console.log(reason)
-})
+// p.then((value) => {
+//   console.log(value)
+// }, (reason) => {
+//   console.log(reason)
+// })
 
 // then 执行后应该返回新的 promise
 // 因为promise的状态失败就不能成功了
-Promise.reject().then(null, (reason) => {
-  return Promise.resolve(100)
-}).then((data) => {
-  console.log(data) // 上班失败还走成功
+// Promise.reject().then(null, (reason) => {
+//   return Promise.resolve(100)
+// }).then((data) => {
+//   console.log(data) // 上班失败还走成功
+// })
+
+// promise中的类型错误
+let p = new Promise(function (resolve, reject) {
+  resolve('ok')
+})
+let promise2 = p.then(() => {
+  // 循环引用，因为自己循环引用
+  return 100
 })
--- day03/promise2.js
+++ day03/promise2.js
@@ -1,3 +1,4 @@
+console.log('方法执行')
 function Promise (executor) {
   let self = this;
   self.value = undefined;
@@ -32,8 +33,31 @@ function Promise (executor) {
 }
 
 // 解析链式调用
-function resolvePromise (x, promise, resolve, reject) {
-
+function resolvePromise (x, promise2, resolve, reject) {
+  console.log(x, promise2)
+  if (x === promise2) {
+    return reject(new TypeError('循环引用'))
+  }
+  if (x !== null && typeof x === 'function' || typeof x === 'object') {
+    try {
+      let then = x.then
+      if (typeof then === 'function') { // 确定是promise
+        then.call(x, function (y) {
+          resolve(y)
+        }, function (r) {
+          reject(r)
+        })
+      } else {
+        // {then: 123}
+        resolve(x)
+      }
+    } catch (error) {
+      reject(error) // 对象上没有then方法
+    }
+  } else {
+    // x是普通值，直接成功了
+    resolve(x)
+  }
 }
 
 Promise.prototype.then = function (onFulfilled, onRejected) {
@@ -45,7 +69,7 @@ Promise.prototype.then = function (onFulfilled, onRejected) {
       // 把then中成功或者失败的函数执行结果取到，判断执行结果是promise还是普通值
       let x = onFulfilled(self.value)
       resolvePromise(x, promise2, resolve, reject)
-      // 如果x 是普通值就让 promise变成成功太
+      // 如果x 是普通值就让 promise变成成功
     }
     if (self.status === 'rejected') {
       let x = onRejected(self.reason)
@@ -61,7 +85,7 @@ Promise.prototype.then = function (onFulfilled, onRejected) {
         let x = onRejected(self.reason)
         resolvePromise(x, promise2, resolve, reject)
       })
-      return promise
+      return promise2
     }
   })
 }
