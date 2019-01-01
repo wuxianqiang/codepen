80e003774054feb5b218f26d381d27d278379b85
wuxianqiang <2631640352@qq.com>
2019-01-01 21:25:27 +0800

    co库的实现原理

--- day04/generator.js
+++ day04/generator.js
@@ -4,22 +4,35 @@
 // 生成器是用来生成迭代器的
 // 什么是迭代器
 
+// let likeArray = {
+//   0: 1,
+//   1: 2,
+//   2: 3,
+//   length: 3,
+//   [Symbol.iterator] () {
+//     let flag = false;
+//     let index = 0;
+//     let that = this;
+//     return {
+//       next () {
+//         return {
+//           done: index === that.length,
+//           value: that[index++]
+//         }
+//       }
+//     }
+//   }
+// }
+
 let likeArray = {
   0: 1,
   1: 2,
   2: 3,
   length: 3,
-  [Symbol.iterator] () {
-    let flag = false;
+  [Symbol.iterator]: function* (params) {
     let index = 0;
-    let that = this;
-    return {
-      next () {
-        return {
-          done: index === that.length,
-          value: that[index++]
-        }
-      }
+    while (index !== this.length) {
+      yield this[index++]
     }
   }
 }
@@ -28,3 +41,13 @@ let arr = [...likeArray]
 console.log(arr)
 
 // 生成器生成迭代器
+
+function* gen() {
+  yield 1; // generator 遇到next会暂停
+  yield 2;
+}
+
+let it = gen()
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
---
+++ day04/generator2.js
@@ -0,0 +1,27 @@
+function* gen() {
+  let a = yield 1;
+  console.log(a)
+}
+
+let it = gen()
+it.next() // 第一次调用next函数时传递的参数是无效的
+it.next(1) // 第二次next 执行的参数会作为第一yield的返回值
+
+
+let fs = require('fs')
+let co = require('co')
+function co (it) {
+  return new Promise((resolve, reject) => {
+    function next () {
+      let {value, done} = it.next()
+      if (!done) {
+        value.then(data => {
+          next(data)
+        }, reject)
+      } else {
+        resolve(value)
+      }
+    }
+    next()
+  })
+}
